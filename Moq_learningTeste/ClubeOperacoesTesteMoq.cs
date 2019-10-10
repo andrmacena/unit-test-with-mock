5f92164ce3b08d4a9aed5a4177af453b5a75834c
Andre dos Santos de Macena <AndreMacena@ibm.com>
2019-10-10 17:16:06 -0300

    Projeto de teste e testes criados

---
+++ Moq_learningTeste/ClubeOperacoesTesteMoq.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Moq_learning;
+
+namespace Moq_learningTeste
+{
+    [TestClass]
+    public class ClubeOperacoesTesteMoq
+    {
+        private Clube _target;
+
+        private Mock<ISocio> _mock;
+
+        [TestMethod]
+        public void CalcularFreteTeste()
+        {
+            _mock = new Mock<ISocio>();
+            _target = new Clube(_mock.Object);
+
+            Socio _socio = new Socio()
+            {
+                SocioID = 1,
+                Nome = "André",
+                Sobrenome = "Macena",
+                Idade = 21,
+                Limite = 5,
+            };
+
+            _mock.Setup(x => x.GetSocio(It.IsAny<int>())).Returns(_socio);
+
+            double esperado = 14;
+
+            double obtido = _target.CalcularFrete(_socio.SocioID);
+            Assert.AreEqual(esperado, obtido);
+        }
+    }
+}
---
+++ Moq_learningTeste/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Moq_learningTeste")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Moq_learningTeste")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("65796a7e-0cd3-403e-b84b-f4c1b4ef5ff8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
