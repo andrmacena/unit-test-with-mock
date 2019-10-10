318f0fd3ecdae4d8e366ad165018a3ac54699c8e
Andre dos Santos de Macena <AndreMacena@ibm.com>
2019-10-10 17:14:38 -0300

    Classe GerenciaSocio.cs alterado extendendo a interface

--- Moq_learning/GerenciaSocio.cs
+++ Moq_learning/GerenciaSocio.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace Moq_learning
 {
-    public class GerenciaSocio
+    public class GerenciaSocio : ISocio
     {
+        public Socio GetSocio(int socioID)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
